dd2318d0255f4267dd93905c2c2045aae2bc7a8d
Fazlı Berk ÖRDEK <berkordek97@gmail.com>
2023-07-04 03:40:05 +0300

    Iterative implemantaion with java

--- src/Solution.java
+++ src/Solution.java
@@ -2,37 +2,40 @@
  * @author fazliberkordek
  */
 //This is a binary search implemantaion with java
-import java.text.*;
-import java.util.*;
-import java.math.*;
-import java.util.regex.*;
-public class Solution{
+
+public class Solution {
 
     //We can implement recursively or iteratively
 
-    //1-Lets do the Recursively firs :
-    public static boolean binarySearchRecursiveley(int[] array,int x,int left,int right){
-        if(left > right){
-            return false;
-        }
-        int mid  = (left + right)/2;
-        if (array[mid]==x){
-            return true;
-        }else if (x<array[mid]){
-            return binarySearchRecursiveley(array,x,left,mid-1);
-        }else {
-            return binarySearchRecursiveley(array,mid + 1,left,right);
-        }
+    //1-Lets do the Recursively first :
+    public static boolean binarySearchRecursiveley(int[] array, int x) {
+        int left = 0;
+        int right = array.length - 1;
 
-      }
-      public static boolean binarySearchRecursiveley(int[] array,int x){
-        return binarySearchRecursiveley(array,x,0,array.length-1);
-      }
-
-    public static class Main {
-        public static void main(String[] args) {
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            if (array[mid] == x) {
+                return true;
+            } else if (x < array[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
 
         }
+        return false;
     }
+
+    public static boolean binarySearchRecursiveley(int[] array, int x) {
+        return binarySearchRecursiveley(array, x, 0, array.length - 1);
+    }
+}
+
+
+public static class Main {
+    public static void main(String[] args) {
+
+    }
+}
 
 }
